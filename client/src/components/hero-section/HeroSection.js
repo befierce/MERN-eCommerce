f4afafe1009a4bfdb27336a4a2ee26540972e9e1
manoj bhatt <enterbhatt123@gmail.com>
2025-03-05 16:59:00 +0530

    made few style changes and added a button to shop

--- client/src/components/hero-section/HeroSection.js
+++ client/src/components/hero-section/HeroSection.js
@@ -1,11 +1,16 @@
 import './HeroSection.css'
-
-const HeroSection = ()=>{
+import Button from '../Button';
+const HeroSection = () => {
     return <>
-    <img alt='action' src="pexels-pixabay-47730.jpg"></img>
-    <div class="image-text">You just practise<br></br>
-        we will deliver<br></br>best class sport products
-    </div>
+        <img alt='action' src="pexels-pixabay-47730.jpg"></img>
+        <div class="image-text-right">You just practise<br></br>
+            we will deliver<br></br>best class sport products
+        </div>
+        <div class="image-text-left">Choose From Variety Of products<br></br>
+            <br></br>
+            <Button className='get_started_button'>START SHOPPING...</Button>
+        </div>
+
     </>
 }
 
